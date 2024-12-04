1f9191c3dd9d72e19068ece6754c459d9d54dd13
Enrique Cervantes <cervantes.enri.0201@gmail.com>
2024-12-03 19:56:51 -0600

    se cambio la url del home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { movieRouter } from "./router/router_movies.js";
 import { corsMiddleware } from "./middleware/cors.js";
 
-const PUERTO = process.env.PORT || 3000;
+const PUERTO = process.env.PORT || 1234;
 const app = express();
 
 app.disable("x-powered-by");
@@ -11,10 +11,10 @@ app.use(express.json());
 
 app.use(corsMiddleware());
 
-app.use("/movies", movieRouter);
+app.use("/", movieRouter);
 
 app.listen(PUERTO, () => {
   console.log(
-    `el servidor esta ecuchando en el el puerto http://localhost:${PUERTO}`
+    `el servidor esta ecuchando en el el puerto http://localhost:${PUERTO}/movies`
   );
 });
